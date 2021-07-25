dda8ac688775f515f7bc55f06c5ea49b092c26e9
Gaurav <hkrgs1234@gmail.com>
2021-07-25 20:00:04 +0530

    Host_preperation aid  file added

--- lfs_10.0/lfs_scripts/section_1/Host_preperation.sh
+++ lfs_10.0/lfs_scripts/section_1/Host_preperation.sh
@@ -2,17 +2,44 @@
 #install all the required software manually
 
 
+# partitioning the hard-disk(using fdisk or gparted )  
+	
+
+#formatting the hard disk ( mkfs or mkfs.ext4)
+
+
+#mount the partition (using mount command)
 
 
 
+#creating directory structure in the $LFS dir
 
 
 
+#adding user (empty user )on the host machine 
+	#groupadd lfs
+	#useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 
-# setting the LFS variable 
-export LFS=/mnt/lfs
+# setting up environment ( creating .bashrc and .profile file in lfs home dir)
+#creating .profile file in lfs user home dir 
+	#cat >~/.profile <<"EOF"
+	#exec env -i HOME=$HOEM 
+	#TERM=$TERM 
+	#PS1='\u:\w\$ ' /bin/bash 
+	#EOF
 
-# partitioning the hard-disk  
+#creating .bashrc file in user's home dir 
+	#cat >~/,bashrc <<"EOF"
+	#set +h
+	#umask 022
+	#LFS=/mnt/lfs
+	#LC_ALL=POSIX
+	#LFS_TGT=$(uname -r)-lfs-linux-gnu
+	#PATH=$LFS/tools:$PATH
+	#export LFS LC_ALL LFS_TGT PATH
+	#EOF
+
+
 
 
 
